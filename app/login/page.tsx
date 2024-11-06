0da489df2c45226aee30e0219524dde25ef234a1
Your Name <marcosralbrecht@gmail.com>
2024-11-05 23:32:41 -0300

    feat: add authenticate with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,15 @@
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { LogInIcon } from "lucide-react";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 import { Button } from "../_components/ui/button";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
       {/* Esquerda */}
@@ -20,9 +27,11 @@ export default function LoginPage() {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant="outline">
-          <LogInIcon className="mr-2" /> Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" /> Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       {/* Direita */}
       <div className="relative h-full w-full">
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
-    <div className="flex w-screen items-center justify-center pt-4">
-      <Button>Hellow world</Button>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName></UserButton>
     </div>
   );
 }
