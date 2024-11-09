67283581b0e130f54133458c36c7012a9e8e5ec2
Your Name <marcosralbrecht@gmail.com>
2024-11-09 00:02:00 -0300

    feat: add transaction type badge

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,3 +1,24 @@
-export default function TransactionsPage() {
-  return <h1>transactions page</h1>;
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { DataTable } from "../_components/ui/data-table";
+import { db } from "../_lib/prisma";
+import { transactionsColumns } from "./_columns";
+
+export default async function TransactionsPage() {
+  //acessar as transaçoes do banco de dados
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+      <div>
+        <DataTable columns={transactionsColumns} data={transactions} />
+      </div>
+    </div>
+  );
 }
