facb795abea6217268627c8b4f6b130368ef9097
Your Name <marcosralbrecht@gmail.com>
2024-11-25 23:58:04 -0300

    feat: adjusted scroll area in transactions

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import AddTransactionButton from "../_components/add-transaction-button";
-import NavBar from "../_components/navbar";
+import Navbar from "../_components/navbar";
 import { DataTable } from "../_components/ui/data-table";
 import { ScrollArea } from "../_components/ui/scroll-area";
 import { db } from "../_lib/prisma";
@@ -19,14 +19,16 @@ export default async function TransactionsPage() {
   });
   return (
     <>
-      <NavBar />
-      <div className="space-y-6 overflow-hidden p-6">
+      <Navbar />
+      <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
+        {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
-        <div>
-          <ScrollArea>
+        {/* CONTAINER SCROLL */}
+        <div className="flex-1 overflow-hidden overflow-y-auto">
+          <ScrollArea className="col-span-2 h-full rounded-md border pb-6">
             <DataTable columns={transactionsColumns} data={transactions} />
           </ScrollArea>
         </div>
