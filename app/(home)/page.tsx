94216bd25929f8fa3a9265144324a00a35814355
Your Name <marcosralbrecht@gmail.com>
2024-11-25 23:41:55 -0300

    feat: ensure pages are correctly scrolled by the user

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,10 @@
 import { auth } from "@clerk/nextjs/server";
 import { getMonth, isMatch } from "date-fns";
 import { redirect } from "next/navigation";
-import NavBar from "../_components/navbar";
+import Navbar from "../_components/navbar";
 import { getDashboard } from "../_data/get-dashboard";
 import { ExpensesPerCategory } from "./_components/expenses-per-category";
+import LastTransactions from "./_components/last-transactions";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import TransactionPieChart from "./_components/transactions-pi-chart";
@@ -37,22 +38,23 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
   const dashboard = await getDashboard(updatedMonth);
   return (
     <>
-      <NavBar />
-      <div className="space-y-6 p-6">
+      <Navbar />
+      <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <div className="grid grid-cols-[2fr,1fr]">
-          <div className="flex flex-col gap-6">
-            <SummaryCards month={updatedMonth} {...dashboard} />
-            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+        <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
+          <div className="flex flex-col gap-6 overflow-hidden">
+            <SummaryCards month={month} {...dashboard} />
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
               <TransactionPieChart {...dashboard} />
               <ExpensesPerCategory
                 expensesPerCategory={dashboard.totalExpensePerCategory}
               />
             </div>
           </div>
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
