819d52752320973e938990b9d1bb03ba378b2713
Your Name <marcosralbrecht@gmail.com>
2024-12-19 23:53:34 -0300

    feat: creating ai report

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,10 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { getMonth, isMatch } from "date-fns";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 import { getDashboard } from "../_data/get-dashboard";
+import AiReportButton from "./_components/ai-report-button";
 import { ExpensesPerCategory } from "./_components/expenses-per-category";
 import LastTransactions from "./_components/last-transactions";
 import SummaryCards from "./_components/summary-cards";
@@ -38,13 +39,22 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
   }
   const dashboard = await getDashboard(updatedMonth);
   const userCanAddTransaction = await canUserAddTransaction();
+  const user = await clerkClient().users.getUser(userId);
   return (
     <>
       <Navbar />
       <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
-          <TimeSelect />
+          <div className="flex items-center gap-3">
+            <AiReportButton
+              month={month}
+              hasPremiumPlan={
+                user.publicMetadata.subscriptionPlan === "premium"
+              }
+            />
+            <TimeSelect />
+          </div>
         </div>
         <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
           <div className="flex flex-col gap-6 overflow-hidden">
